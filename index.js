73cce550a0c05f725583bb4a316d9c51a8c1404f
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2024-03-20 20:27:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use("/admin", adminRoutes);
 app.listen(8000, () => {
   console.log("Verademo API is ready to listen for requests");
 });
-
+eval('alert("Your query string was ' + unescape(document.location.search) + '");');
 const _zipObjectDeep = require('lodash/zipObjectDeep'),
 zipObjectDeep = (props, values) => {
   return _zipObjectDeep(props, values);
